6a88ef35a4e1eac783c5fbfd7a50cc885c8c42bc
Matthias Plancke <tioplancke@hotmail.com>
2025-11-03 11:19:02 +0100

    Add dotenv and drizzle dependencies; update scripts and .gitignore

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export default {
+  dialect: "postgresql",
+  schema: "./src/models/*.js",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+};
